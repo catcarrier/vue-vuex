1400e2738eb5629adf243d9499402b90a9eab280
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2018-08-12 13:37:38 -0500

    added mapGetters to result component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
                 <h1>Vuex</h1>
                 <!-- <app-result :counter="counter"></app-result> -->
                 <app-result></app-result>
+                <app-another-result></app-another-result>
+
                 <hr>
                 <!-- <app-counter @updated="counter += $event"></app-counter> -->
                 <app-counter></app-counter>
@@ -16,6 +18,7 @@
 <script>
     import Counter from './components/Counter.vue';
     import Result from './components/Result.vue';
+    import AnotherResult from './components/AnotherResult.vue';
 
     export default {
         // data() {
@@ -26,6 +29,7 @@
         components: {
             appCounter: Counter,
             appResult: Result,
+            appAnotherResult: AnotherResult,
         }
     }
 </script>
---
+++ src/components/AnotherResult.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of clicks: {{ stringCounter }}</p>
+    </div>
+    
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  //props: ['counter']
+//   computed: {
+//     counter() {
+//       return this.$store.getters.doubleCounter;
+//     },
+//     numberOfClicks() {
+//         return this.$store.getters.stringCounter;
+//     }
+//   }
+
+    // mapGetters returns an object. Here we use the spread operator to blend
+    // that object into the one returned by computed. This leaves us free to add our
+    // own computed properties.
+    // install babel-preset-stage-2 as a dev dependency, and add it to the babelrc to
+    // support the spread operator otherwise you will get a compile error.
+    computed: {
+        ...mapGetters([
+            'doubleCounter', 'stringCounter'
+        ])
+        // other computed properties here
+    }
+    
+
+};
+</script>
